650968eeb4a02019b8337828a2f2fc62759fbdf8
svettuff <arkadii.nagogaw@gmail.com>
2025-06-15 09:59:57 +0200

    style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { Buffer } from 'buffer';
